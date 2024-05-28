e4f4f12d4f09b77af9de6f9902c99e3282f1967c
CodeYouness <ylijassi@gmail.com>
2024-05-28 12:49:41 +0200

    added img with src link in js

--- js/script.js
+++ js/script.js
@@ -2,6 +2,9 @@ const { createApp } = Vue
 
 createApp({
     data() {
-        return { message: 'Hello Vue' }
+        return {
+            img: 'https://www.cavouresoterica.it/wp-content/uploads/2017/12/Pillola-rossa-o-pillola-blu.jpg',
+            message: 'Hello Vue'
+        }
     }
 }).mount('#output')
